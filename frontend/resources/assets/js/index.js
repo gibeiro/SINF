c3be91f4a02a859fe272dd0a138cccc643fc56ee
unknown <rui.pedro.csoares@gmail.com>
2017-11-08 22:42:24 +0000

    Fixed CORS issue.

--- frontend/resources/assets/js/index.js
+++ frontend/resources/assets/js/index.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $.ajax({
         type: 'GET',
-		headers: { 'Access-Control-Allow-Origin': '*' },
         url: 'http://localhost:49822/api/clientes',
 		datatype: 'application/json',
         success: function (data) {
--- primavera/Controllers/ClientesController.cs
+++ primavera/Controllers/ClientesController.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Web.Http;
 using FirstREST.Lib_Primavera.Model;
 
+
 namespace FirstREST.Controllers
 {
     public class ClientesController : ApiController
