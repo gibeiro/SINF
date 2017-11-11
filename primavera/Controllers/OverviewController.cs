c9ca420c51c1276a461a75bd2b1a19365cf2b532
Nuno Martins <nunojorge90@hotmail.com>
2017-11-11 16:32:46 +0000

    changed class of top artigos

--- primavera/Controllers/OverviewController.cs
+++ primavera/Controllers/OverviewController.cs
@@ -39,7 +39,7 @@ namespace FirstREST.Controllers
         // GET: api/overview/products
         [HttpGet]
         [ActionName("Products")]
-        public IEnumerable<Lib_Primavera.Model.Artigo> Products()
+        public IEnumerable<Lib_Primavera.Model.LinhaDocVenda> Products()
         {
             return Lib_Primavera.PriIntegration.TopArtigos();
         }
--- primavera/Lib_Primavera/PriIntegration.cs
+++ primavera/Lib_Primavera/PriIntegration.cs
@@ -388,24 +388,26 @@ namespace FirstREST.Lib_Primavera
 
         }
         //Artigos que geram mais lucro
-        public static List<Model.Artigo> TopArtigos()
+        public static List<Model.LinhaDocVenda> TopArtigos()
         {
             StdBELista objList;
 
-            List<Model.Artigo> listArtigos = new List<Model.Artigo>();
+            List<Model.LinhaDocVenda> listArtigos = new List<Model.LinhaDocVenda>();
 
             if (PriEngine.InitializeCompany(FirstREST.Properties.Settings.Default.Company.Trim(), FirstREST.Properties.Settings.Default.User.Trim(), FirstREST.Properties.Settings.Default.Password.Trim()) == true)
             {
 
-                objList = PriEngine.Engine.Consulta("SELECT LinhasDoc.Artigo, LinhasDoc.Descricao, SUM(LinhasDoc.PrecUnit * LinhasDoc.Quantidade) AS Price FROM LinhasDoc GROUP BY Artigo,Descricao ORDER BY Price DESC");
+                objList = PriEngine.Engine.Consulta(
+                    "SELECT LinhasDoc.Artigo, LinhasDoc.Descricao, SUM(LinhasDoc.PrecUnit * LinhasDoc.Quantidade) AS Price FROM LinhasDoc GROUP BY Artigo,Descricao ORDER BY Price DESC"
+                    );
 
                 while (!objList.NoFim())
                 {
-                    listArtigos.Add(new Model.Artigo
+                    listArtigos.Add(new Model.LinhaDocVenda
                     {
                         CodArtigo = objList.Valor("Artigo"),
                         DescArtigo = objList.Valor("Descricao"),
-                        Preco = objList.Valor("Price")
+                        TotalLiquido = objList.Valor("Price")
                     });
                     objList.Seguinte();
 
