31c630bed5873f5a8b0ad7621e66d83c6faf300b
Nuno Martins <nunojorge90@hotmail.com>
2017-11-11 01:56:00 +0000

    add cabecdoc

---
+++ primavera/Lib_Primavera/Model/CabecDoc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class CabecDoc
+    {
+        public string id
+        {
+            get;
+            set;
+        }
+
+        public DateTime Data
+        {
+            get;
+            set;
+        }
+
+        public string Entidade
+        {
+            get;
+            set;
+        }
+
+        public string NumDoc
+        {
+            get;
+            set;
+        }
+
+        public string NumContribuinte
+        {
+            get;
+            set;
+        }
+
+        public string TotalMerc
+        {
+            get;
+            set;
+        }
+
+        public string TotalIVA
+        {
+            get;
+            set;
+        }
+
+        public List<Model.LinhaDocVenda> LinhasDoc
+        {
+            get;
+            set;
+        }
+
+    }
+}
