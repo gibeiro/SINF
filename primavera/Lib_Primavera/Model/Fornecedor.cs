8e837cc387e2763c57b8fe5cd21d5915e34e3f87
Nuno Martins <nunojorge90@hotmail.com>
2017-11-11 15:24:46 +0000

    added fornecedor

---
+++ primavera/Lib_Primavera/Model/Fornecedor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class Fornecedor
+    {
+        public string CodFornecedor
+        {
+            get;
+            set;
+        }
+
+        public string Nome
+        {
+            get;
+            set;
+        }
+
+        public double Morada
+        {
+            get;
+            set;
+        }
+
+        public string CodigoPostal
+        {
+            get;
+            set;
+        }
+
+        public string Localidade
+        {
+            get;
+            set;
+        }
+    }
+}
