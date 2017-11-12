2e3c69726de42847696016cb5bdf9014bccc4d74
Nuno Martins <nunojorge90@hotmail.com>
2017-11-12 13:06:11 +0000

    fix param name

--- primavera/Controllers/ProductController.cs
+++ primavera/Controllers/ProductController.cs
@@ -15,9 +15,9 @@ namespace FirstREST.Controllers
         [HttpGet]
         [ActionName("Volume")]
         [Route("product/volume/{id?}")]
-        public List<Lib_Primavera.Model.Custom.SalesVol> Volume(string cod = null,string y = "2016")
+        public List<Lib_Primavera.Model.Custom.SalesVol> Volume(string id = null,string y = "2016")
         {
-            return Lib_Primavera.PriIntegration.SalesVolYear(cod, Int32.Parse(y));
+            return Lib_Primavera.PriIntegration.SalesVolYear(id, Int32.Parse(y));
         }
 
         // GET: api/product/list
