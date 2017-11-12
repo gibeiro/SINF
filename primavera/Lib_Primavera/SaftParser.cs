1dfca4b3cec24ec5e9068d49052be880bbce94a7
Nuno Martins <nunojorge90@hotmail.com>
2017-11-12 17:18:48 +0000

    added parser

--- primavera/Lib_Primavera/SaftParser.cs
+++ primavera/Lib_Primavera/SaftParser.cs
@@ -5,16 +5,25 @@ using System.Web;
 using System.Xml;
 using System.IO;
 using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace FirstREST.Lib_Primavera
 {
     public class SaftParser
     {
-        public XmlDocument saft;
+        XmlDocument saft;
         public SaftParser(string path)
         {
             saft = new XmlDocument();
-            saft.Load(path);
+            saft.Load(path); 
+        }
+
+        public List<Model.Artigo> getArtigos()
+        {
+            List<Model.Artigo> artigos = new List<Model.Artigo>();
+
+            return artigos;
         }
 
     }
