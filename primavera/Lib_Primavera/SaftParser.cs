57e18171a2dc7a786a82fb4992c7d0689a383b19
Nuno Martins <nunojorge90@hotmail.com>
2017-11-12 16:52:28 +0000

    added parse..

---
+++ primavera/Lib_Primavera/SaftParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.IO;
+using System.Text;
+
+namespace FirstREST.Lib_Primavera
+{
+    public class SaftParser
+    {
+        public XmlDocument saft;
+        public SaftParser(string path)
+        {
+            saft = new XmlDocument();
+            saft.Load(path);
+        }
+
+    }
+}
