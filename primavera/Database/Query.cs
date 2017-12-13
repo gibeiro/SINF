9704c3c520ef5877b4652b275073c45cc36b59e1
gibeiro <goncalo.ribeiro@fe.up.pt>
2017-12-13 04:57:03 +0000

    fixed sales volume

--- primavera/Database/Query.cs
+++ primavera/Database/Query.cs
@@ -395,10 +395,10 @@ namespace FirstREST.Database
             List<object> volume = new List<object>();
 
             SqliteDB.com.CommandText =
-                @"select count(*) as sales,
+                @"select count(*) as sales, date, 
                 cast(julianday(date) - julianday(@1) as integer) as day
                 from invoice
-                where date between @2 and @3
+                where date between @1 and @2
                 group by day
                 order by day asc";
 
