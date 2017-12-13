9d78054ec543e10efa302375019fa0cc8d9fad31
unknown <rui.pedro.csoares@gmail.com>
2017-12-13 03:58:58 +0000

    Product

--- frontend/resources/assets/js/product.js
+++ frontend/resources/assets/js/product.js
@@ -1,9 +1,97 @@
-/*  /api/product/list
-    /api/product/get?id=codigo
-    /api/product/volume?id=?&y=?*/
+var growth_chart_data;
+var volume_chart_data;
 
+Date.prototype.ymd = function() {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
+
+    return '' + this.getFullYear() +"-"+ (mm>9 ? '' : '0') + mm +"-"+ (dd>9 ? '' : '0') + dd;
+};
+
+
+function volume_processed_data(data){
+    var volume_data = [];
+    var currentDay = 0;
+	var date_parts = $('#date_i').val().split('-');
+    var first_date = new Date(date_parts[0],date_parts[1]-1,date_parts[2]);
+    for(var i=0 ; i<data.length ; i++){
+		while(currentDay!=data[i].day){
+			var date = new Date(date_parts[0],date_parts[1]-1,date_parts[2]);
+            date.setDate(first_date.getDate()+currentDay);
+            volume_data.push({day:currentDay, gross:0, date: date.ymd()});
+            currentDay++;
+        }
+        volume_data.push(data[i]);
+        currentDay++;
+    }
+    return volume_data;
+}
+
+function save_processed_data(data){
+    var volume_data = [];
+    var currentDay = 0;
+	var date_parts = $('#date_i').val().split('-');
+    var first_date = new Date(date_parts[0],date_parts[1]-1,date_parts[2]);
+    for(var i=0 ; i<data.length ; i++){
+		while(currentDay!=data[i].day){
+			var date = new Date(date_parts[0],date_parts[1]-1,date_parts[2]);
+            date.setDate(first_date.getDate()+currentDay);
+            volume_data.push({day:currentDay, sales:0, date: date.ymd()});
+            currentDay++;
+        }
+        volume_data.push(data[i]);
+        currentDay++;
+    }
+    return volume_data;
+}
+
+function sales_by_month(data){
+    LABELS = ["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
+    var months=[];
+    var currentElement=-1;
+    $.each(data,function(index,element){
+        var parts = element.date.split('-');
+        if(newElement(months,parts[1]-1, parts[0])) {
+            months.push({month: parts[1]-1, year: parts[0], sales: element.sales, label: LABELS[parts[1]-1]});
+            currentElement++;
+        }else{
+            months[currentElement].sales += element.sales;
+        }
+    });
+	console.log(months);
+    return months;
+}
+
+function volume_by_month(data){
+    LABELS = ["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
+    var months=[];
+    var currentElement=-1;
+    $.each(data,function(index,element){
+        var parts = element.date.split('-');
+        if(newElement(months,parts[1]-1, parts[0])) {
+            months.push({month: parts[1]-1, year: parts[0], gross: element.gross, label: LABELS[parts[1]-1]});
+            currentElement++;
+        }else{
+            months[currentElement].gross += element.gross;
+        }
+    });
+	console.log(months);
+    return months;
+}
+
+
+function newElement(array, month, year){
+    for(var i=0;i<array.length;i++){
+        if(array[i].month == month && array[i].year == year){
+            return false;
+        }
+    }
+    return true;
+}
 
 $(document).ready(function () {
+    var date_i = $('#date_i').val();
+    var date_f = $('#date_f').val();
     //get id of product
     var id = $('#id').attr('content');
     /**
@@ -24,110 +112,51 @@ $(document).ready(function () {
     });
 
     /**
+     * Get product sales
+     */
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:49822/api/product/sales?id='+id+'&from='+date_i+'&to='+date_f,
+        datatype: 'application/json',
+        success: function (data) {
+			growth_chart_data = save_processed_data(data);
+            sales_volume_chart(sales_by_month(growth_chart_data), 'month');
+        }
+    });
+	
+	/**
      * Get product volume
      */
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:49822/api/product/volume?id='+id+'&from=2016-01-01&to=2016-12-31',
+        url: 'http://localhost:49822/api/product/volume?id='+id+'&from='+date_i+'&to='+date_f,
         datatype: 'application/json',
         success: function (data) {
-            sales_volume_chart(data);
-            //profit_volume_chart(data);
+			volume_chart_data = volume_processed_data(data);
+            volume_chart(volume_by_month(volume_chart_data), 'month');
         }
     });
-});
+	
+	
+	$('input[type=radio]').on('change',function(){
+	//val can be:  year, month, week, day
+		switch($('input[type=radio]:checked').val()){
+			case 'year':
+				break;
+			case 'month':
+				volume_chart(volume_by_month(volume_chart_data), 'month');
+				sales_volume_chart(sales_by_month(growth_chart_data), 'month');
+				break;
+			case 'week':
+				break;
+			case 'day':
+				volume_chart(volume_chart_data, 'day');
+				sales_volume_chart(growth_chart_data, 'day');
+				break;
+		}
+	});
 
-/*var ctx = document.getElementById('myPieChart').getContext('2d');
-var myPieChart = new Chart(ctx,{
-    type: 'doughnut',
-    data: {
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Items in hand',
-            'To be recieved'
-        ],
-        datasets: [{
-            data: [70, 30],
-            backgroundColor: [
-                'rgba(240,100,100,0.7)',
-                'rgba(240,240,100,0.7)',
-                'rgba(100,100,240,0.7)',
-                'rgba(100,240,100,0.7)'
-            ]
-        }]
-    },
-    options: {
-        centertext: '100'
-    }
 });
-
-ctx = document.getElementById('myPieChart2').getContext('2d');
-var myPieChart2 = new Chart(ctx,{
-    type: 'bar',
-    data: {
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Red',
-            'Yellow',
-            'Blue',
-            'Green'
-        ],
-        datasets: [{
-            data: [20, 50, 20,10],
-            backgroundColor: [
-                'rgba(240,100,100,0.7)',
-                'rgba(240,240,100,0.7)',
-                'rgba(100,100,240,0.7)',
-                'rgba(100,240,100,0.7)'
-            ]
-        }]
-    },
-    options: {
-        responsive: true,
-        title:{
-            display:true,
-            text:'Income Expenses'
-        },
-        tooltips: {
-            mode: 'index',
-            intersect: false,
-        },
-        hover: {
-            mode: 'nearest',
-            intersect: true
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-
-ctx = document.getElementById('myPieChart3').getContext('2d');
-var myPieChart3 = new Chart(ctx,{
-    type: 'pie',
-    data: {
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Red',
-            'Yellow',
-            'Blue',
-            'Green'
-        ],
-        datasets: [{
-            data: [20, 50, 20,10],
-            backgroundColor: [
-                'rgba(240,100,100,0.7)',
-                'rgba(240,240,100,0.7)',
-                'rgba(100,100,240,0.7)',
-                'rgba(100,240,100,0.7)'
-            ]
-        }]
-    }
-});*/
 
 function profit_chart(product) {
     profit = (product.pvp - product.pcm).toFixed(2);
@@ -155,7 +184,7 @@ function profit_chart(product) {
     });
 }
 
-function sales_volume_chart(volume) {
+function sales_volume_chart(volume, type) {
     labels=[];
     data=[];
     var colors = [
@@ -165,11 +194,15 @@ function sales_volume_chart(volume) {
         'rgba(100,240,100,0.4)',
         'rgba(100,240,240,0.4)'
     ];
-    LABELS = ["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
+	
+	
     $.each(volume, function(index,element){
-        labels.push(LABELS[index]);
-        data.push(element.gross);
+        if (type == 'day') labels.push(element.date);
+		if (type == 'month') labels.push(element.label);
+        data.push(element.sales);
     });
+	
+	
     ctx = document.getElementById('myLineChart').getContext('2d');
     var myLineChart = new Chart(ctx, {
         type: 'bar',
@@ -206,7 +239,7 @@ function sales_volume_chart(volume) {
     });
 }
 
-function profit_volume_chart(volume) {
+function volume_chart(volume, type) {
     labels=[];
     data=[];
     var colors = [
@@ -216,11 +249,13 @@ function profit_volume_chart(volume) {
         'rgba(100,240,100,0.4)',
         'rgba(100,240,240,0.4)'
     ];
-    LABELS = ["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
+	
     $.each(volume, function(index,element){
-        labels.push(LABELS[index]);
-        data.push(element.Profit.toFixed(2));
+        if (type == 'day') labels.push(element.date);
+		if (type == 'month') labels.push(element.label);
+        data.push(element.gross);
     });
+	
     ctx = document.getElementById('myLineChart2').getContext('2d');
     var myLineChart2 = new Chart(ctx, {
         type: 'line',
