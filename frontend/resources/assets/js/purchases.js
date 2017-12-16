27dbd10e8994831cdfe19f27167c4940d7d13b7c
unknown <rui.pedro.csoares@gmail.com>
2017-12-16 19:26:15 +0000

    Purchase view

--- frontend/resources/assets/js/purchases.js
+++ frontend/resources/assets/js/purchases.js
@@ -1,25 +1,145 @@
+var graph_data;
+var expenditure_graph_data;
+
+var conjoined_data = [];
+
+Date.prototype.ymd = function() {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
+
+    return '' + this.getFullYear() +"-"+ (mm>9 ? '' : '0') + mm +"-"+ (dd>9 ? '' : '0') + dd;
+};
+
+function purchases_data(data){
+    var volume_data = [];
+    var currentDay = 0;
+	var date_parts = $('#date_i').val().split('-');
+    var first_date = new Date(date_parts[0],date_parts[1]-1,date_parts[2]);
+    for(var i=0 ; i<data.length ; i++){
+		while(currentDay!=data[i].day){
+			var date = new Date(date_parts[0],date_parts[1]-1,date_parts[2]);
+            date.setDate(first_date.getDate()+currentDay);
+            volume_data.push({day:currentDay, purchases:0, date: date.ymd()});
+            currentDay++;
+        }
+        volume_data.push(data[i]);
+        currentDay++;
+    }
+    return volume_data;
+}
+
+function expenditure_data(data){
+    var volume_data = [];
+    var currentDay = 0;
+	var date_parts = $('#date_i').val().split('-');
+    var first_date = new Date(date_parts[0],date_parts[1]-1,date_parts[2]);
+    for(var i=0 ; i<data.length ; i++){
+		while(currentDay!=data[i].day){
+			var date = new Date(date_parts[0],date_parts[1]-1,date_parts[2]);
+            date.setDate(first_date.getDate()+currentDay);
+            volume_data.push({day:currentDay, cost:0, date: date.ymd()});
+            currentDay++;
+        }
+        volume_data.push(data[i]);
+        currentDay++;
+    }
+    return volume_data;
+}
+
+function purchases_by_month(data){
+    LABELS = ["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
+    var months=[];
+    var currentElement=-1;
+    $.each(data,function(index,element){
+        var parts = element.date.split('-');
+        if(newElement(months,parts[1]-1, parts[0])) {
+            months.push({month: parts[1]-1, year: parts[0], purchases: element.purchases, label: LABELS[parts[1]-1]});
+            currentElement++;
+        }else{
+            months[currentElement].purchases += element.purchases;
+        }
+    });
+    return months;
+}
+
+function expenditures_by_month(data){
+    LABELS = ["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
+    var months=[];
+    var currentElement=-1;
+    $.each(data,function(index,element){
+        var parts = element.date.split('-');
+        if(newElement(months,parts[1]-1, parts[0])) {
+            months.push({month: parts[1]-1, year: parts[0], cost: element.cost, label: LABELS[parts[1]-1]});
+            currentElement++;
+        }else{
+            months[currentElement].cost += element.cost;
+        }
+    });
+    return months;
+}
+
+function newElement(array, month, year){
+    for(var i=0;i<array.length;i++){
+        if(array[i].month == month && array[i].year == year){
+            return false;
+        }
+    }
+    return true;
+}
 
 $(document).ready(function () {
+	
+    var date_i = $('#date_i').val();
+    var date_f = $('#date_f').val();
+	
     $('#example').DataTable(
         {"dom": "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
         "<'row'<'col-sm-12'tr>>" +
         "<'row'<'col-sm-5'i><'col-sm-7'p>>"}
     );
     /**
-     * Get cost earns and profit
+     * Get volume
      */
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:49822/api/overview/growth?y=2016',
+        url: 'http://localhost:49822/api/purchases/volume?from='+date_i+'&to='+date_f,
         datatype: 'application/json',
         success: function (data) {
-            growth = data;
-            growth_chart(data);
+            graph_data = purchases_data(data);			
+			var temp = purchases_by_month(data);			
+			for (var i = 0; i < temp.length; i++) conjoined_data.push([temp[i], 0]);
+		}
+    });
+	
+	/**
+     * Get expenditure
+     */
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:49822/api/purchases/expenditure?from='+date_i+'&to='+date_f,
+        datatype: 'application/json',
+        success: function (data) {
+			expenditure_graph_data = expenditure_data(data);
+			var temp = expenditures_by_month(data);
+			for (var i = 0; i < conjoined_data.length; i++) conjoined_data[i] = ([conjoined_data[i][0], temp[i]]);
+			growth_chart(conjoined_data, 'month');
+        }
+    });
+	
+	/**
+     * Get product purchases
+     */
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:49822/api/purchases/latest',
+        datatype: 'application/json',
+        success: function (data) {
+			fillTable(data);
         }
     });
 });
 
-function growth_chart(data) {
+function growth_chart(data, type) {
     var labels = [];
 
     var data_cost = [];
@@ -34,10 +154,10 @@ function growth_chart(data) {
     ];
     LABELS = ["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
     $.each(data, function(index,element){
-        labels.push(LABELS[index]);
-        data_cost.push(element.Cost.toFixed(0));
-        data_earn.push(element.Earn.toFixed(0));
-        data_profit.push(element.Profit.toFixed(0));
+        if (type == 'day') labels.push(element[0].date);
+		if (type == 'month') labels.push(element[0]	.label);
+        if (element[0].purchases != undefined) data_cost.push(element[0].purchases.toFixed(0));
+		if (element[1].cost != undefined) data_earn.push(element[1].cost.toFixed(0) / 1000);
     });
     ctx = document.getElementById('myLineChart').getContext('2d');
     var myLineChart = new Chart(ctx, {
@@ -51,7 +171,7 @@ function growth_chart(data) {
                     backgroundColor: colors[0]
                 },
                 {
-                    label: "Budget",
+                    label: "Expenditure (in 1000s of €)",
                     data: data_earn,
                     backgroundColor: colors[1]
                 }
@@ -61,7 +181,7 @@ function growth_chart(data) {
             responsive: true,
             title: {
                 display: true,
-                text: 'Purchases / Budget'
+                text: 'Purchases / Expenditure'
             },
             tooltips: {
                 mode: 'index',
@@ -80,6 +200,25 @@ function growth_chart(data) {
             }
         }
     });
+}
+
+
+function fillTable (data){
+	
+    $.each(data, function(index,element){
+        $('#example2 tbody').append('<tr>\n' +
+            '<td>' + element.productcode + '</td>\n' +
+            '<td>' + element.productname + '</td>\n' +
+            '<td>' + element.quantity + '</td>\n' +
+            '<td>' + element.totalcost + '</td>\n' +
+            '</tr>');
+    });
+    $('#example2').DataTable(
+        {"dom": "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'i><'col-sm-7'p>>"}
+    );
+	
 }
 
 
--- frontend/resources/views/purchases.blade.php
+++ frontend/resources/views/purchases.blade.php
@@ -10,8 +10,8 @@
                     <h2>Purchases</h2>
                 </div>
                 <div class="col-md-6 col-xs-12 text-right" style="padding: 50px 10px 20px 0;">
-                    <label>Data de Início: <input type="date" name="date_i" value="2017-01-01"></label>
-                    <label>Data de Fim: <input type="date" name="date_f" value="2017-12-31"></label>
+                    <label>Data de Início: <input type="date" id="date_i" value="2016-01-01"></label>
+                    <label>Data de Fim: <input type="date" id="date_f" value="2016-12-31"></label>
                 </div>
             </div>
             <div class="row">
@@ -28,94 +28,17 @@
             <div class="text-center">
                 <h1>Last Purchases</h1>
             </div>
-            <table id="example" class="table table-striped table-bordered text-left" cellspacing="0" width="100%">
+            <table id="example2" class="table table-striped table-bordered text-left" cellspacing="0" width="100%">
                 <thead>
                 <tr>
-                    <th>Supplier</th>
-                    <th>Item</th>
-                    <th>Price</th>
-                    <th>Date</th>
-                    <th>Arrived</th>
+                    <th>Code</th>
+                    <th>Name</th>
+                    <th>Quantity (in units)</th>
+                    <th>Price (in €)</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>Supplier 1</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 2</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 3</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 4</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 5</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 6</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 7</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 8</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 9</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 10</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
-                <tr>
-                    <td>Supplier 11</td>
-                    <td>Item xxx</td>
-                    <td>23,23</td>
-                    <td>2017/04/25</td>
-                    <td>2017/05/01 15:33:42</td>
-                </tr>
+                
                 </tbody>
             </table>
         </div>
